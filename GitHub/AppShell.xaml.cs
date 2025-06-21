39dc4517ece8e1fa87ec1a6c395ff10b92e133c3
Diego Cortez Carreon <c.diego2004@gmail.com>
2025-06-20 21:56:52 -0500

    APP TERMINADA

--- GitHub/AppShell.xaml.cs
+++ GitHub/AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace GitHub
+﻿using GitHub;
+
+namespace GitHub
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("inicio", typeof(MainPage));
             Routing.RegisterRoute("cineSeries", typeof(CinePage));
             Routing.RegisterRoute("videojuegos", typeof(VideojuegosPage));
             Routing.RegisterRoute("musica", typeof(Musica));
---
+++ GitHub/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace GitHub
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnStartClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//cineSeries");
+        }
+    }
+}
