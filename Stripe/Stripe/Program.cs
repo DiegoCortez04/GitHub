cf3a20be1f48735fe041520deabc3b76c908ebc0
SantosLeon12 <zzzbeyn@gmail.com>
2025-07-24 18:38:37 -0600

    apipago

---
+++ Stripe/Stripe/Program.cs
@@ -0,0 +1,42 @@
+using Stripe;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"]; //Llamada de la api key que se encuentra en el .json
+Console.WriteLine("Stripe Key cargada: " + StripeConfiguration.ApiKey);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
